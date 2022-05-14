c06777072902a0210bfe539476cd03e1cd5fef13
Rob Docherty <robert.g.docherty@gmail.com>
2022-05-14 21:18:43 +0100

    ReSharper mods

--- Pinger/PingEngine.cs
+++ Pinger/PingEngine.cs
@@ -7,32 +7,31 @@ namespace Pinger
     {
         internal static void Start()
         {
-            ConsoleColor usual = Console.ForegroundColor;
+            var usual = Console.ForegroundColor;
 
-            long shortest = long.MaxValue;
-            long longest = long.MinValue;
+            var shortest = long.MaxValue;
+            var longest = long.MinValue;
             long totalPings = 0;
             long successfulPings = 0;
             long failedPings = 0;
-            decimal successRate;
             decimal avgTime = 0;
             long totalTime = 0;
             
 
-            string data = "All our lives we sweat and save.";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 10000;
-            const int snoozeTime = 3000;
-            const string Brizzy = "8.8.8.8";
+            const string Data = "All our lives we sweat and save.";
+            var buffer = Encoding.ASCII.GetBytes(Data);
+            const int Timeout = 10000;
+            const int SnoozeTime = 3000;
+            const string RemoteServer = "8.8.8.8";
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"Host: {Brizzy}, Timeout: {timeout}, Packet Size: {buffer.Length}, Snooze Time: {snoozeTime}");
+            Console.WriteLine($"Host: {RemoteServer}, Timeout: {Timeout}, Packet Size: {buffer.Length}, Snooze Time: {SnoozeTime}");
             Console.ForegroundColor = usual;
 
             while (true)
             {
 
-                var status = PingHost(Brizzy, timeout, buffer);
+                var status = PingHost(RemoteServer, Timeout, buffer);
                 totalPings++;
 
                 if (status.Success)
@@ -60,7 +59,7 @@ namespace Pinger
                     }
                 }
 
-                successRate = Math.Round(((decimal)successfulPings / (decimal)totalPings) * (decimal)100, 1);
+                var successRate = Math.Round((successfulPings / (decimal)totalPings) * 100, 1);
 
                 if (!status.Success)
                 {
@@ -75,7 +74,7 @@ namespace Pinger
                 Console.WriteLine($"{successRate}% R{status.PingTime}. T{totalPings} P{successfulPings} F{failedPings}. A{avgTime} S{shortest} L{longest}");
 
                 Console.ForegroundColor = usual;
-                Thread.Sleep(snoozeTime);
+                Thread.Sleep(SnoozeTime);
             }
         }
 
@@ -87,7 +86,7 @@ namespace Pinger
             try
             {
                 pinger = new Ping();
-                PingReply reply = pinger.Send(nameOrAddress, timeout, buffer);
+                var reply = pinger.Send(nameOrAddress, timeout, buffer);
                 ps.Success = reply.Status == IPStatus.Success;
                 ps.PingTime = reply.RoundtripTime;
             }
--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -2,7 +2,7 @@
 {
     public static class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             PingEngine.Start();
         }
