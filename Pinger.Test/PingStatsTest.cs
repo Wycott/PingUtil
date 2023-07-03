a1b951d76ef3f8fa046ed0ed4f6ee1ee1ea66ae4
Wycott <robert.g.docherty@gmail.com>
2023-07-03 21:36:01 +0100

    Tweak test name and remove duplicate assertion

--- Pinger.Test/PingStatsTest.cs
+++ Pinger.Test/PingStatsTest.cs
@@ -3,17 +3,16 @@ namespace Pinger.Test
     public class PingStatsTest
     {
         [Fact]
-        public void WhenSetup_ThenShouldBeAsExpected()
+        public void WhenSetup_ThenItShouldBeAsExpected()
         {
             // Arrange
             const int expectedPingTime = 20;
             const bool expectedSuccessFlag = true;
 
             // Act
-            var ps = new PingStats() { PingTime = expectedPingTime, Success = expectedSuccessFlag };
+            var ps = new PingStats { PingTime = expectedPingTime, Success = expectedSuccessFlag };
 
             // Assert
-            Assert.Equal(expectedPingTime, ps.PingTime);
             Assert.Equal(expectedPingTime, ps.PingTime);
             Assert.Equal(expectedSuccessFlag, ps.Success);
         }
